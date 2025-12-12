a1218405b60f52b1f16f3cf959500383f5f541c9
MarvinB <marvin.nunez@correounivalle.edu.co>
2025-12-12 13:58:15 -0500

    update

--- index.py
+++ index.py
@@ -46,6 +46,8 @@ dataset = ImageFolderSafe("data", transform=transform)
 print("Clases:", dataset.classes)
 
 # ---------------- DIVISIÓN TRAIN/TEST ----------------
+torch.manual_seed(12)
+
 train_size = int(0.8 * len(dataset))
 test_size = len(dataset) - train_size
 train_dataset, test_dataset = random_split(dataset, [train_size, test_size])
